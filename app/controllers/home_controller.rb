82af16f582df7c642ec80e6d96751f923db976ea
Ana <abarrios.chavez@gmail.com>
2016-12-10 15:30:47 -0700

    Fix History show all games

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,7 +4,7 @@ class HomeController < ApplicationController
   end
 
   def history
-    @games = Game.where(home: current_user)
+    @games = all_games(current_user)
   end
 
   def log
@@ -18,5 +18,15 @@ class HomeController < ApplicationController
     return 'L' if home_score < opponent_score
   end
   helper_method :winner_result
+
+  private
+  
+  def all_games(current_user)
+    home_games1 = Game.where(home: current_user)
+    home_games = home_games1.map {|h| {home: h.home, opponent: h.opponent, game_date: h.game_date, home_score: h.home_score, opponent_score: h.opponent_score}}
+    opponent_games1 = Game.where(opponent: current_user)
+    opponent_games = opponent_games1.map {|h| {home: h.opponent, opponent: h.home, game_date: h.game_date, home_score: h.opponent_score, opponent_score: h.home_score}}
+    home_games + opponent_games
+  end
 
 end
